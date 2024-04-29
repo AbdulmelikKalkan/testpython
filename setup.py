21f40eba23c0fb78ca234947c703d15c69926b30
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2024-04-29 08:52:59 +0300

    update

--- setup.py
+++ setup.py
@@ -1,21 +1,8 @@
-#!/usr/bin/env python3
-
 from setuptools import setup, find_packages
 
 setup(
-    name='greetings',
-    version='0.1.0',
-    packages=find_packages(),  # Paketinizin içerdiği modülleri listeleyin
-    url='https://github.com/AbdulmelikKalkan/testpython',  # GitHub deponuzun URL'sini girin
-    license='MIT',
-    author='Abdulmelik Kalkan',
-    author_email='abdulmelikkalkan@gmail.com',
-    description='Örnek Python paketi',
-    long_description='Bu paket, Python paket geliştirmeye başlamak için bir örnektir.',
-    install_requires=[],  # Gerekli bağımlılıkları listeleyin
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
+    name='greetings',  # Replace with your desired library name
+    version='1.0.0',  # Replace with your library version
+    packages=find_packages(),
+    description='A simple greetings library',  # Add a description
 )
